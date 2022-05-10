c4f7cbe156c02790902fe65c83873eb8cb5a5c6f
Luke-A-C-Roberts <lrobertt44@gmail.com>
2022-05-10 12:56:52 +0100

    Player and Game Functionality Created

--- OOP_Assessment_2/Game.cs
+++ OOP_Assessment_2/Game.cs
@@ -10,6 +10,32 @@ namespace OOP_Assessment_2
             Dice diceObj = new Dice(6);
 
             humanPlayerObj.Name = gameuiObj.AskUserName();
+
+            static int GenerateScore(int[] diceVals)
+            {
+                Array.Sort(diceVals);
+                int[] numAmmounts = new int[6];
+                for (int i = 0; i < 6; i++)
+                {
+                    numAmmounts[i] = diceVals.Count(s => s == i);
+                }
+                int tempScore = 0;
+                if (numAmmounts.Max() == 5)
+                {
+                    tempScore = 12;
+                }
+                else if (numAmmounts.Max() == 4)
+                {
+                    tempScore = 6;
+                }
+                else if (numAmmounts.Max() == 3)
+                {
+                    tempScore = 3;
+                }
+                return tempScore;
+            }
+            
+            
         }
     }
 }
--- OOP_Assessment_2/Player.cs
+++ OOP_Assessment_2/Player.cs
@@ -22,13 +22,27 @@ namespace OOP_Assessment_2
             Score = 0;
             Name = "";
         }
+        public static HumanPlayer operator +(HumanPlayer oldHumanPlayer, int i)
+        {
+            HumanPlayer newHumanPlayer = new HumanPlayer();
+            newHumanPlayer.Score = oldHumanPlayer.Score + i;
+            return newHumanPlayer;
+        }
     }
+
     public class AIPlayer : Player
     {
         public AIPlayer()
         {
             Score = 0;
             Name = "Computer";
+        }
+
+        public static AIPlayer operator +(AIPlayer oldAIPlayer, int i)
+        {
+            AIPlayer newAIPlayer = new AIPlayer();
+            newAIPlayer.Score = oldAIPlayer.Score + i;
+            return newAIPlayer;
         }
     }
 }
